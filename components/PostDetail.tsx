9ea3541a4875fe8960d06bce6ef9c990357c9865
songmin <tft0720@gmail.com>
2023-01-15 10:03:26 +0900

    useMutation 사용

--- components/PostDetail.tsx
+++ components/PostDetail.tsx
@@ -1,6 +1,6 @@
 import { PostsState } from '@/pages';
 import React from 'react';
-import { isError, useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
 
 type CommentState = {
   postId: number;
@@ -15,6 +15,13 @@ async function fetchComments(postId: PostsState['id']) {
   return response.json();
 }
 
+async function deletePost(postId: PostsState['id']) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${postId}`, {
+    method: 'DELETE',
+  });
+  return response.json();
+}
+
 export default function PostDetail({ post }: { post: PostsState }) {
   const { data, isLoading, isError, error } = useQuery<CommentState[]>(['comments', post.id], () =>
     fetchComments(post.id)
@@ -22,6 +29,8 @@ export default function PostDetail({ post }: { post: PostsState }) {
   // useQuery fetching
   // isError, isLoading 결과 설명
   // 쿼리키
+
+  const deleteMutation = useMutation((postId: number) => deletePost(postId));
 
   if (isError) {
     return (
@@ -36,10 +45,21 @@ export default function PostDetail({ post }: { post: PostsState }) {
     return <h1>loading....</h1>;
   }
 
+  const onDeleteClick = (postId: number) => {
+    deleteMutation.mutate(postId);
+  };
+
   return (
     <>
       <h3 style={{ color: 'blue' }}>{post.title}</h3>
-      <button type='button'>Delete</button>
+      <button type='button' onClick={() => onDeleteClick(post.id)}>
+        Delete
+      </button>
+      <div style={{ padding: 10, fontSize: '20px', fontWeight: 'bold' }}>
+        {deleteMutation.isError && <p style={{ color: 'red' }}>Error deleting the post</p>}
+        {deleteMutation.isLoading && <p style={{ color: 'purple' }}>Deleting the post</p>}
+        {deleteMutation.isSuccess && <p style={{ color: 'green' }}>Success deleteing the post</p>}
+      </div>
       <button type='button'>Update Title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
